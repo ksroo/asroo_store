64b91803139b59d2c23c733cda44c570a2c5cd6e
walidMeem <w.mahmoud@meemdev.com>
2024-01-28 23:27:39 +0200

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 
 import 'package:asroo_store/asroo_store_app.dart';
+import 'package:asroo_store/core/app/env.variables.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
 
 //  "current_key": "AIzaSyBDzognjD6pwp6oKKOoEkklgOunZo3W-fs" ==> apiKey
 //  "mobilesdk_app_id": "1:255535904497:android:bf1c974c2689a199431b50" ==> appId
@@ -23,5 +27,9 @@ void main() async {
         )
       : await Firebase.initializeApp();
 
-  runApp(const AsrooStoreApp());
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
+  ).then((_) {
+    runApp(const AsrooStoreApp());
+  });
 }
