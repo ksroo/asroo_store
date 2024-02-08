328b1758bccb199c59c920f7a870db6efbd5ea51
walidMeem <w.mahmoud@meemdev.com>
2024-02-09 01:30:41 +0200

    get all products admin bloc

--- lib/features/admin/add_products/presentation/screens/add_products_screen.dart
+++ lib/features/admin/add_products/presentation/screens/add_products_screen.dart
@@ -1,21 +1,34 @@
 import 'package:asroo_store/core/common/widgets/admin_app_bar.dart';
+import 'package:asroo_store/core/di/injection_container.dart';
 import 'package:asroo_store/core/style/colors/colors_dark.dart';
+import 'package:asroo_store/features/admin/add_products/presentation/bloc/get_all_admin_product/get_all_admin_product_bloc.dart';
 import 'package:asroo_store/features/admin/add_products/presentation/refactors/add_products_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddProductsScreen extends StatelessWidget {
   const AddProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: ColorsDark.mainColor,
-      appBar: AdminAppBar(
-        title: 'Products',
-        isMain: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<GetAllAdminProductBloc>()
+            ..add(
+              const GetAllAdminProductEvent.getAllProducts(isNotLoading: true),
+            ),
+        ),
+      ],
+      child: const Scaffold(
         backgroundColor: ColorsDark.mainColor,
+        appBar: AdminAppBar(
+          title: 'Products',
+          isMain: true,
+          backgroundColor: ColorsDark.mainColor,
+        ),
+        body: AddProductsBody(),
       ),
-      body: AddProductsBody(),
     );
   }
 }
