28c5dc19b94a3d17c63126e2bcbc3c96a18f0f66
walidMeem <w.mahmoud@meemdev.com>
2024-02-19 01:42:00 +0200

    fix app bar main screen

--- lib/features/customer/main/presentation/refactors/main_customer_app_bar.dart
+++ lib/features/customer/main/presentation/refactors/main_customer_app_bar.dart
@@ -1,11 +1,14 @@
 import 'package:asroo_store/core/common/animations/animate_do.dart';
 import 'package:asroo_store/core/common/widgets/custom_linear_button.dart';
 import 'package:asroo_store/core/common/widgets/text_app.dart';
+import 'package:asroo_store/core/enums/nav_bar_enum.dart';
 import 'package:asroo_store/core/extensions/context_extension.dart';
 import 'package:asroo_store/core/language/lang_keys.dart';
 import 'package:asroo_store/core/style/fonts/font_weight_helper.dart';
 import 'package:asroo_store/core/style/images/app_images.dart';
+import 'package:asroo_store/features/customer/main/presentation/cubit/main_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -15,34 +18,43 @@ class MainCustomerAppBar extends StatelessWidget
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<MainCubit>();
     return AppBar(
       automaticallyImplyLeading: false,
       backgroundColor: context.color.mainColor,
       elevation: 0,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          CustomFadeInRight(
-            duration: 800,
-            child: TextApp(
-              text: context.translate(LangKeys.chooseProducts),
-              theme: context.textStyle.copyWith(
-                fontSize: 20.sp,
-                fontWeight: FontWeightHelper.bold,
-                color: context.color.textColor,
-              ),
-            ),
-          ),
-          CustomFadeInLeft(
-            duration: 800,
-            child: CustomLinearButton(
-              onPressed: () {},
-              child: Center(
-                child: SvgPicture.asset(AppImages.search),
-              ),
-            ),
-          )
-        ],
+      title: BlocBuilder(
+        bloc: cubit,
+        builder: (context, state) {
+          if (cubit.navBarEnum == NavBarEnum.home) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomFadeInRight(
+                  duration: 800,
+                  child: TextApp(
+                    text: context.translate(LangKeys.chooseProducts),
+                    theme: context.textStyle.copyWith(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeightHelper.bold,
+                      color: context.color.textColor,
+                    ),
+                  ),
+                ),
+                CustomFadeInLeft(
+                  duration: 800,
+                  child: CustomLinearButton(
+                    onPressed: () {},
+                    child: Center(
+                      child: SvgPicture.asset(AppImages.search),
+                    ),
+                  ),
+                )
+              ],
+            );
+          }
+          return const SizedBox.shrink();
+        },
       ),
     );
   }
