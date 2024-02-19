37f9344d6d47dc112c8766d2fe4c912405a827c2
walidMeem <w.mahmoud@meemdev.com>
2024-02-19 02:10:50 +0200

    status code 400

---
+++ lib/core/utils/app_logout.dart
@@ -0,0 +1,25 @@
+import 'package:asroo_store/core/di/injection_container.dart';
+import 'package:asroo_store/core/extensions/context_extension.dart';
+import 'package:asroo_store/core/routes/app_routes.dart';
+import 'package:asroo_store/core/service/shared_pref/pref_keys.dart';
+import 'package:asroo_store/core/service/shared_pref/shared_pref.dart';
+import 'package:flutter/widgets.dart';
+
+class AppLogout {
+  factory AppLogout() {
+    return _instance;
+  }
+
+  AppLogout._();
+
+  static final AppLogout _instance = AppLogout._();
+
+  Future<void> logout() async {
+    final context = sl<GlobalKey<NavigatorState>>().currentState!.context;
+    await SharedPref().removePreference(PrefKeys.accessToken);
+    await SharedPref().removePreference(PrefKeys.userId);
+    await SharedPref().removePreference(PrefKeys.userRole);
+    if (!context.mounted) return;
+    await context.pushNamedAndRemoveUntil(AppRoutes.login);
+  }
+}
