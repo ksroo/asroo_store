6dd3ecfd40aded6e3a791565e86df3718f46da83
walidMeem <w.mahmoud@meemdev.com>
2024-02-22 22:00:34 +0200

    fix import

--- lib/features/customer/search/presentation/refactors/search_body.dart
+++ lib/features/customer/search/presentation/refactors/search_body.dart
@@ -1,5 +1,4 @@
 import 'package:asroo_store/features/customer/search/presentation/widgets/filter_buttons.dart';
-import 'package:asroo_store/features/customer/search/presentation/widgets/search_for_data_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,14 +9,10 @@ class SearchBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 20.h),
-      child: Column(
+      child: const Column(
         children: [
           // filter buttons
           FilterButtons(),
-
-          // SizedBox(height: 20.h),
-
-          // SearchFormDataIcon(),
         ],
       ),
     );
