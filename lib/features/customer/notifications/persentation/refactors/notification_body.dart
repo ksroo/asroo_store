09a3bd1e0ddbd8f167c2963b0586fcab8ee2cd76
walidMeem <w.mahmoud@meemdev.com>
2024-03-08 07:00:48 +0200

    notification database done

--- lib/features/customer/notifications/persentation/refactors/notification_body.dart
+++ lib/features/customer/notifications/persentation/refactors/notification_body.dart
@@ -1,4 +1,14 @@
+import 'package:asroo_store/core/common/widgets/text_app.dart';
+import 'package:asroo_store/core/extensions/context_extension.dart';
+import 'package:asroo_store/core/routes/app_routes.dart';
+import 'package:asroo_store/core/service/shared_pref/pref_keys.dart';
+import 'package:asroo_store/core/service/shared_pref/shared_pref.dart';
+import 'package:asroo_store/core/style/fonts/font_weight_helper.dart';
+import 'package:asroo_store/core/utils/app_strings.dart';
+import 'package:asroo_store/features/customer/notifications/data/models/notification_model.dart';
+import 'package:asroo_store/features/customer/notifications/persentation/controller/notifcaiton_controller.dart';
 import 'package:asroo_store/features/customer/notifications/persentation/widgets/notification_item.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,14 +19,76 @@ class NotificationBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-      child: ListView.separated(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return const NotificationItem();
+      child: StreamBuilder<List<NotificationModel>>(
+        stream: NotificationController().getNotificationList(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            );
+          } else if (snapshot.data!.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.notification_add,
+                    color: context.color.textColor,
+                    size: 150,
+                  ),
+                  TextApp(
+                    text: 'You Not Has Notifications Now',
+                    theme: context.textStyle.copyWith(
+                      fontWeight: FontWeightHelper.bold,
+                      fontSize: 16.sp,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+
+          return ListView.separated(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return NotificationItem(
+                body: snapshot.data![index].body,
+                title: snapshot.data![index].title,
+                isSeen: snapshot.data![index].isSeen,
+                createdAt: snapshot.data![index].createdAt,
+                onTapSelected: () async {
+                  await FirebaseFirestore.instance
+                      .collection(usersCollection)
+                      .doc(SharedPref().getInt(PrefKeys.userId).toString())
+                      .collection(notificationCollection)
+                      .doc(snapshot.data![index].notificationId)
+                      .update({'isSeen': true});
+
+                  if (snapshot.data![index].productId != -1) {
+                    if (!context.mounted) return;
+                    await context.pushName(
+                      AppRoutes.productDetails,
+                      arguments: snapshot.data![index].productId,
+                    );
+                  }
+                },
+                onTapRemoved: () async {
+                  await FirebaseFirestore.instance
+                      .collection(usersCollection)
+                      .doc(SharedPref().getInt(PrefKeys.userId).toString())
+                      .collection(notificationCollection)
+                      .doc(snapshot.data![index].notificationId)
+                      .delete();
+                },
+              );
+            },
+            separatorBuilder: (context, index) => SizedBox(
+              height: 10.h,
+            ),
+          );
         },
-        separatorBuilder: (context, index) => SizedBox(
-          height: 10.h,
-        ),
       ),
     );
   }
--- lib/features/customer/notifications/persentation/widgets/notification_item.dart
+++ lib/features/customer/notifications/persentation/widgets/notification_item.dart
@@ -7,21 +7,36 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 class NotificationItem extends StatelessWidget {
-  const NotificationItem({super.key});
+  const NotificationItem({
+    required this.title,
+    required this.body,
+    required this.createdAt,
+    required this.isSeen,
+    required this.onTapSelected,
+    required this.onTapRemoved,
+    super.key,
+  });
+
+  final String title;
+  final String body;
+  final String createdAt;
+  final bool isSeen;
+  final VoidCallback onTapSelected;
+  final VoidCallback onTapRemoved;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: EdgeInsets.zero,
       isThreeLine: true,
-      onTap: () {},
+      onTap: onTapSelected,
       leading: Container(
         height: 50.h,
         width: 50.w,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           border: Border.all(
-            color: Colors.white,
+            color: isSeen ? Colors.grey : Colors.white,
             width: 1.5,
           ),
         ),
@@ -35,11 +50,11 @@ class NotificationItem extends StatelessWidget {
       ),
       //Title
       title: TextApp(
-        text: 'title',
+        text: title,
         theme: context.textStyle.copyWith(
           fontSize: 12.sp,
           fontWeight: FontWeightHelper.medium,
-          color: Colors.white,
+          color: isSeen ? Colors.grey : Colors.white,
         ),
       ),
       subtitle: Column(
@@ -48,31 +63,31 @@ class NotificationItem extends StatelessWidget {
           SizedBox(height: 5.h),
           //Body
           TextApp(
-            text: 'Body',
+            text: body,
             theme: context.textStyle.copyWith(
               fontSize: 12.sp,
               fontWeight: FontWeightHelper.medium,
-              color: Colors.white,
+              color: isSeen ? Colors.grey : Colors.white,
             ),
           ),
           SizedBox(height: 5.h),
           //created at
           TextApp(
-            text: 'Created At',
+            text: createdAt,
             theme: context.textStyle.copyWith(
               fontSize: 12.sp,
               fontWeight: FontWeightHelper.medium,
-              color: Colors.white,
+              color: isSeen ? Colors.grey : Colors.green,
             ),
           ),
           SizedBox(height: 5.h),
           Divider(
-            color: Colors.white,
+            color: isSeen ? Colors.grey : Colors.white,
           ),
         ],
       ),
       trailing: GestureDetector(
-        onTap: () {},
+        onTap: onTapRemoved,
         child: const Icon(
           Icons.close,
           color: Colors.yellow,
