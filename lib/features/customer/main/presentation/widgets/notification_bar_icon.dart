f79ce2aacadf7908837ccee5c6c2720f7e0c372c
walidMeem <w.mahmoud@meemdev.com>
2024-03-08 06:23:25 +0200

    notificaiton icon logic

--- lib/features/customer/main/presentation/widgets/notification_bar_icon.dart
+++ lib/features/customer/main/presentation/widgets/notification_bar_icon.dart
@@ -2,6 +2,8 @@ import 'package:asroo_store/core/common/widgets/text_app.dart';
 import 'package:asroo_store/core/extensions/context_extension.dart';
 import 'package:asroo_store/core/style/fonts/font_weight_helper.dart';
 import 'package:asroo_store/core/style/images/app_images.dart';
+import 'package:asroo_store/features/customer/notifications/data/models/notification_model.dart';
+import 'package:asroo_store/features/customer/notifications/persentation/controller/notifcaiton_controller.dart';
 import 'package:badges/badges.dart' as badge;
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
@@ -15,29 +17,40 @@ class NotificationBarIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return badge.Badge(
-      showBadge: true,
-      badgeAnimation: const badge.BadgeAnimation.scale(),
-      position: badge.BadgePosition.topEnd(end: -4.w, top: -8.h),
-      badgeContent: TextApp(
-        text: '9',
-        theme: context.textStyle.copyWith(
-          color: Colors.white,
-          fontWeight: FontWeightHelper.bold,
-          fontSize: 11.sp,
-        ),
-        textAlign: TextAlign.center,
-      ),
-      badgeStyle: badge.BadgeStyle(
-        badgeColor: Colors.pink,
-        padding: EdgeInsets.all(5.h),
-        elevation: 0,
-      ),
-      child: SvgPicture.asset(
-        AppImages.notificationIcon,
-        color: isSelected ? context.color.navBarSelectedTab : Colors.grey,
-        height: 25.h,
-      ).animate(target: isSelected ? 1 : 0).scaleXY(end: 1.2),
+    return StreamBuilder<List<NotificationModel>>(
+      stream: NotificationController().getNotificationList(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SizedBox.shrink();
+        }
+
+        final numberOfNotifi =
+            snapshot.data!.where((e) => e.isSeen == false).length;
+        return badge.Badge(
+          showBadge: numberOfNotifi != 0,
+          badgeAnimation: const badge.BadgeAnimation.scale(),
+          position: badge.BadgePosition.topEnd(end: -4.w, top: -8.h),
+          badgeContent: TextApp(
+            text: numberOfNotifi >= 9 ? '9+' : numberOfNotifi.toString(),
+            theme: context.textStyle.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeightHelper.bold,
+              fontSize: 11.sp,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          badgeStyle: badge.BadgeStyle(
+            badgeColor: Colors.pink,
+            padding: EdgeInsets.all(5.h),
+            elevation: 0,
+          ),
+          child: SvgPicture.asset(
+            AppImages.notificationIcon,
+            color: isSelected ? context.color.navBarSelectedTab : Colors.grey,
+            height: 25.h,
+          ).animate(target: isSelected ? 1 : 0).scaleXY(end: 1.2),
+        );
+      },
     );
   }
 }
